117bd8668fa2eb84422915cf84b7562384dbde00
KazimirAlvis <superkore.mail@gmail.com>
2026-02-13 08:09:40 -0800

    Release 1.3.65

--- blocks/two-column-text/build/index.asset.php
+++ blocks/two-column-text/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'a9d490e44b18184571c4');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '579d2974456b5f9231cd');

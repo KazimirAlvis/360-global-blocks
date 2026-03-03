4ab3bfc99b238da61bfbfb2248c4ae5e79caa278
KazimirAlvis <superkore.mail@gmail.com>
2026-03-03 12:20:31 -0800

    Adjust Two Column Text paragraph and list spacing

--- blocks/two-column-text/build/index.asset.php
+++ blocks/two-column-text/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '0450a822ca9c634d30f6');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'b670e52ccf97a858a621');
